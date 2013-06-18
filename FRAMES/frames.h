9d1c0e90ee87bfce46a69c1e355f36a8bf478982
Ovidiu - Dan Bogat <odb9088@gmail.com>
2013-06-18 16:56:35 +0200

    deleted

--- FRAMES/frames.h
+++
@@ -1,8 +0,0 @@
-#ifndef _frames_h
-#define _frames_h
-#include "list.h"
-
-int alloc_frame(List *frames);
-int free_frame(List *frames, int frame_number);
-
-#endif /*frames.h*/
--- FRAMES/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "frames.h"
-#include "list.h"
-
-int main(int argc, char *argv[])
-{
-  
-  return 0;
-}
